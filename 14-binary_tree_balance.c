7d3ecd82726a5c32c43287f3f98ba9f95d50f530
modernfuthark <timothys1738@gmail.com>
2020-07-22 19:43:51 -0400

    [N] Added file for task 14 to directory

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,52 @@
+#include "binary_trees.h"
+size_t Recursive_helper(const binary_tree_t *tree);
+
+/**
+ * binary_tree_balance - Gets the balance factor of a binary tree
+ * @tree: Binary tree
+ * Return: Balance factor of tree 'tree'
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left = 0, right = 0;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return (left - right);
+}
+
+/**
+ * binary_tree_height - Return the height of a binary tree
+ * @tree: Binary tree
+ * Return: Height of tree 'tree'
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (Recursive_helper(tree));
+}
+
+/**
+ * Recursive_helper - Helper function to count recuirsivly
+ * @tree: Binary tree
+ * Return: Height of tree 'tree'
+ */
+size_t Recursive_helper(const binary_tree_t *tree)
+{
+	size_t Height_left, Height_right;
+
+	if (!tree)
+		return (0);
+	Height_left = Recursive_helper(tree->left);
+	Height_right = Recursive_helper(tree->right);
+
+
+	if (Height_left > Height_right) /* Get the longest height */
+		return (Height_left + 1);
+	return (Height_right + 1);
+}

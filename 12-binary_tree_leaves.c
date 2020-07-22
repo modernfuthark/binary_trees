e291804ab4c46b2f3aaaf666416a8976df11425b
modernfuthark <timothys1738@gmail.com>
2020-07-22 19:19:26 -0400

    [N] Added file for task 12 to directory

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Returns the amount of leaves attached to a root
+ * @tree: Binary tree
+ * Return: Number of leaves
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t Total = 0;
+
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right && tree->parent)
+		return (1);
+
+	Total = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	return (Total);
+}

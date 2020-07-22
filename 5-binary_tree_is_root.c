4941a7133000c29e49356f4c8096ead446175481
modernfuthark <timothys1738@gmail.com>
2020-07-21 22:28:01 -0400

    [N] Added file for task 5 to directory

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - Checks if a node is a leaf in a binary tree
+ * @node: Node to check
+ * Return: 1 if node is a leaf, 0 if not.
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node && !node->parent && node->left && node->right)
+		return (1);
+	return (0);
+}

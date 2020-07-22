3c25bd3db388214168d0ef86e0a0c251907d8717
modernfuthark <timothys1738@gmail.com>
2020-07-22 19:28:50 -0400

    [^] Removed unneeded check for parent

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,7 +12,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right && tree->parent)
+	if (!tree->left && !tree->right)
 		return (1);
 
 	Total = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);

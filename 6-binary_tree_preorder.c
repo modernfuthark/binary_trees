1b44d80b3d03037a53ce521fc14ae10d07147d92
modernfuthark <timothys1738@gmail.com>
2020-07-21 22:46:22 -0400

    [^] Fixed if statement

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,7 +8,7 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree || !func)
+	if (tree && func)
 	{
 		func(tree->n); /* Run function on first node */
 		binary_tree_preorder(tree->left, func);

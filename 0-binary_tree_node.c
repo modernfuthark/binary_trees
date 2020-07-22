97b0753eaaa352adc42e86675c6ea756b5effb81
modernfuthark <timothys1738@gmail.com>
2020-07-21 21:46:29 -0400

    [N] Added file for task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a new node in a binary tree
+ * @parent: Parent node
+ * @value: Value inside new node
+ * Return: New node, NULL on error
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (!parent)
+		return (NULL);
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	new->n = value;
+
+	return (new);
+}

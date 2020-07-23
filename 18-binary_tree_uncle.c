28b4fc777ea850148e96cfe4402e1df1e0cc09bb
modernfuthark <timothys1738@gmail.com>
2020-07-22 20:54:20 -0400

    [N] Added file for task 18 to directory

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Finds the uncle of a node
+ * @node: Tree node
+ * Return: Uncle of node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+	else
+		return (node->parent->parent->left);
+}

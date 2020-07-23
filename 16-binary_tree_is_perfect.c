a64b22aa3b8cecffea3054b2aec144b00662df8b
modernfuthark <timothys1738@gmail.com>
2020-07-22 20:14:38 -0400

    [N] Added check for left and right nodes

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,8 +8,10 @@ size_t Recursive_helper(const binary_tree_t *tree);
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (!tree && tree->left && tree->right)
+	if (!tree)
 		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
 
 	return (binary_tree_balance(tree->left) == binary_tree_balance(tree->right));
 }
